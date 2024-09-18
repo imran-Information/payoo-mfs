823be868d2378d7dd6bc774dfcd5fd801bcff438
imranhossen21 <imranhossen0586@gmail.com>
2024-09-18 22:46:55 +0600

    file added

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-login').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // phone number value access
+    const phoneNumber = document.getElementById('phone-number').value;
+
+
+
+
+
+
+})
