38884ce7328e075b9c9d599483579345503d9001
imranhossen21 <imranhossen0586@gmail.com>
2024-09-19 02:42:45 +0600

    cash out money added

--- js/home.js
+++ js/home.js
@@ -13,12 +13,32 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
 
         const updateBalance = mainBalanceNumber + moneyInputNumber;
         mainBalance = document.getElementById('main-balance').innerText = updateBalance;
-        
+
     } else {
         alert('Wrong Pin Number')
     }
 
+});
 
+document.getElementById('cash-out-money-btn').addEventListener('click', function (event) {
+    event.preventDefault();
 
+    const cashOutInput = document.getElementById('cash-out-amount-input').value;
+    const cashOutInputNumber = parseInt(cashOutInput);
+
+    const cashOutPinNumber = document.getElementById('cash-out-pin-number-input').value;
+
+    if (cashOutPinNumber === '1234') {
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const mainBalanceNumber = parseInt(mainBalance);
+
+        const updateBalance = mainBalanceNumber - cashOutInputNumber;
+
+        document.getElementById('main-balance').innerText = updateBalance;
+    }
+    else {
+        alert('Wrong Pin number')
+    }
 
 })
+
