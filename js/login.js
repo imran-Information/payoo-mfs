39bdae2efca775d5db86018d228c243c02a6b849
imranhossen21 <imranhossen0586@gmail.com>
2024-09-19 00:05:06 +0600

    home file create

--- js/login.js
+++ js/login.js
@@ -1,8 +1,16 @@
 document.getElementById('btn-login').addEventListener('click', function (event) {
     event.preventDefault();
 
-    // phone number value access
+    // phone number & pin number value access
     const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if (phoneNumber === '01868634721' && pinNumber === "1234") {
+        window.location.href= "home.html"
+    }else{
+        alert('Wrong Phone or Pin Number')
+    }
+
 
 
 
