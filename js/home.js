4679cee6ebdc7b6618e715122f72965a84e77a8b
imranhossen21 <imranhossen0586@gmail.com>
2024-09-19 01:35:03 +0600

    money added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
+document.getElementById('add-money-btn').addEventListener('click', function (event) {
+    event.preventDefault();
 
+    // phone & pin Number access 
+    const moneyInput = document.getElementById('money-amount-input').value;
+    const pinNumber = document.getElementById('pin-number-input').value;
+
+    if (pinNumber === '1234') {
+        // main balance access
+        let mainBalance = document.getElementById('main-balance').innerText;
+        const mainBalanceNumber = parseInt(mainBalance);
+        const moneyInputNumber = parseInt(moneyInput)
+
+        const updateBalance = mainBalanceNumber + moneyInputNumber;
+        mainBalance = document.getElementById('main-balance').innerText = updateBalance;
+        
+    } else {
+        alert('Wrong Pin Number')
+    }
+
+
+
+
+})
